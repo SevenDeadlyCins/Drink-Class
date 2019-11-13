5051ec7c22db0acc55be8f2b747656b2281c1b73
CaptainMauve <56097242+CaptainMauve@users.noreply.github.com>
2019-11-13 15:55:57 -0800

    Update Drink.h

--- Drink.h
+++ Drink.h
@@ -8,14 +8,15 @@ using namespace std;
 //Class declaration
 class Drink
 {
-private:
+public:
 	string name;
 	Recipe rec;
 	string alc_cont;
+	string description;
 	//visit num_vis;
 	//Picture pic;
 
-public:
+
 	Drink();
 	~Drink();
 	Drink(string drinkName, Recipe recip, string content);
