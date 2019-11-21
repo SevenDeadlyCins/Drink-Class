4fa095ddef1bd2cc32efd62bfa82b10d52e0da1b
CaptainMauve <56097242+CaptainMauve@users.noreply.github.com>
2019-11-20 16:46:02 -0800

    Update myrecipe.cpp

--- myrecipe.cpp
+++ myrecipe.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 //Class implementation
-//Recipe tempRecipe;
 Recipe::Recipe()
 {}
 
@@ -17,93 +16,9 @@ Recipe::~Recipe()
 {}
 
 /*
-void Recipe::readRecipe()
-{
-	string line;
-	string drinkName;
-	ifstream fin;
-	fin.open("File.txt");
-	if (!fin)
-		cout << "The input file can't be opened.\n";
-
-	else
-	{
-		getline(fin, line);
-		if (line == "Drink")
-		{
-			getline(fin, line);
-			drinkName = line;
-
-			while (getline(fin, line))
-			{
-				if (line == "End")
-					break;
-
-				else
-					recipeList.push_back(line);
-			}
-		}
-	}
-}
-
-void Recipe::displayRecipe()
-{
-	for(size_t i = 0; i < recipeList.size(); i++)
-	{
-		cout << "Step " << i + 1 << ": ";
-		cout << recipeList[i] << endl;
-	}
-	cout << endl;
-}
-
-void Recipe::readMaterials()
-{
-	string line;
-	ifstream fin;
-	fin.open("File.txt");
-	if (!fin)
-		cout << "The input file can't be opened.\n";
-
-	else
-	{
-		while(getline(fin, line))
-		{	
-			if (line == "Materials")
-			{
-				while (getline(fin, line))
-				{
-					if (line == "End")
-						break;
-
-					else
-						reqMaterials.push_back(line);
-				}
-			}
-		}
-	}
-}
-
-void Recipe::displayMaterials()
-{
-	for (size_t i = 0; i < reqMaterials.size(); i++)
-	{
-		cout << "Material " << i + 1 << ": ";
-		cout << reqMaterials[i] << endl;
-	}
-}
-
 void Recipe::addIngredient(Ingredient newIngredient)
 {
 	ingredientList.push_back(newIngredient);
-}
-
-void Recipe::displayIngredients()
-{
-	for (size_t i = 0; i < ingredientList.size(); i++)
-	{
-		cout << "Ingredient " << i + 1 << ": ";
-		cout << ingredientList[i].getName() << endl;
-	}
 }
 */
 
@@ -115,112 +30,36 @@ void Recipe::addIngredient(Ingredient newIngredient)
 */
 
 /*
-void Recipe::readDrinks()
+int Recipe::getIngredientListSize()
 {
-
-	string line;
-	string drinkName;
-	string ingredientName;
-	string stype;
-	type drinktype;
-	string sprice;
-	price dPrice;
-	ifstream fin;
-	int proof;
-
-	fin.open("File.txt");
-
-	if (!fin)
-		cout << "The input file can't be opened.\n";
-
-	else
-	{
-
-
-
-
-		while (!EOF)
-		{
-			getline(fin, line);
-			if (line == "Drink")
-			{
-				getline(fin, line);
-				drinkName = line;
-			}
-
-			getline(fin, line);
-			while (line == "Ingredients")
-			{
-				
-				getline(fin, line);
-				ingredientName = line;
-				getline(fin, line);
-				stype = line;
-				if (line == "liquid")
-					drinktype = liquid;
-				else
-					drinktype = garnish;
-				getline(fin, line);
-				proof = stoi(line);
-				getline(fin, line);
-				sprice = line;
-				if (line == "low")
-					dPrice = low;
-				else if (line == "mid")
-					dPrice = mid;
-				else
-					dPrice = high;
-
-				Ingredient tempIngredient(ingredientName, drinktype, proof, dPrice);
-
-
-				tempRecipe.addIngredient(tempIngredient);
-
-				getline(fin, line);
-
-			}
-
-			while (getline(fin, line))
-			{
-				if (line == "Materials")
-				{
-					while (getline(fin, line))
-					{
-						if (line == "End")
-							break;
-
-						else
-							tempRecipe.reqMaterials.push_back(line);
-					}
-				}
-			}
-
-			getline(fin, line);
-			if (line == "Steps")
-			{
-				while (getline(fin, line))
-					if (line == "End")
-						break;
-
-					else
-					{
-						tempRecipe.recipeList.push_back(line);
-					}
-
-			}
-			getline(fin, line);
-			if (line == "Alcohol Content")
-				getline(fin, line);
-			Drink tempDrink(drinkName, tempRecipe, line);
-
-		}
-
-
-
-
-
-
-	}
+	return ingredientList.size();
 }
 */
 
+void Recipe::displayMaterials()
+{
+	for (size_t i = 0; i < reqMaterials.size(); i++)
+	{
+		cout << "Material " << i + 1 << ": ";
+		cout << reqMaterials[i] << endl;
+	}
+}
+
+void Recipe::displayRecipe()
+{
+	for (size_t i = 0; i < recipeList.size(); i++)
+	{
+		cout << "Step " << i + 1 << ": ";
+		cout << recipeList[i] << endl;
+	}
+	cout << endl;
+}
+
+void Recipe::displayIngredients()
+{
+	for (size_t i = 0; i < ingredientList.size(); i++)
+	{
+		cout << "Ingredient " << i + 1 << ": ";
+		cout << ingredientList[i].getName() << endl;
+	}
+}
